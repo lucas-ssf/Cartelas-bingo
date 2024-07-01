dcaeaf4e53eff7a11f52fa6932752af158d7d50e
Lucas Sales Fonseca <143020372+lucas-ssf@users.noreply.github.com>
2024-07-01 08:34:52 -0300

    Add files via upload

--- bingo.py
+++ bingo.py
@@ -12,22 +12,28 @@ for i in range(numero_cartelas):
     ident = input('\tEntre com o id: ')
     for j in range(numero_elementos):
         lista.append(int(input(f'\tElemento {j}: ')))
-    cartela = [0,lista,ident]
+    cartela = [0, lista, ident]
     lista_cartelas.append(cartela)
-    
+
 print('\n\nSuas cartelas:')
-for i,cartela in enumerate(lista_cartelas):
+for i, cartela in enumerate(lista_cartelas):
     print(f'{i}.Cartela {cartela[2]}: {cartela[1]}')
-    
 
 input('Pressione Enter...')
 from time import sleep
+
 sleep(2)
-    
-print('\n\n')
+
+import platform
+from os import system
+
 i = 0
 lista = []
-while(True):
+while (True):
+    if platform.system() == 'Windows':
+        system("cls")
+    else:
+        system("clear")
     print(f'Partida {i}:\nNúmeros sorteados: {lista_sorteados[-1::-1]}')
     numero_sorteado = int(input('Digite o número sorteado: '))
     lista_sorteados.append(numero_sorteado)
@@ -35,12 +41,12 @@ while(True):
         if numero_sorteado in cartela[1]:
             cartela[0] += 1
         if cartela[0] == numero_elementos:
-            print('BINGO >> ', end = ' ')
+            print('BINGO >> ', end=' ')
         print(f'Cartela {cartela[2]}: {cartela[0]} | {[num for num in cartela[1] if num in lista_sorteados]}')
     print('\n')
     lista_cartelas.sort(reverse=True)
     i += 1
-    
+
     if numero_sorteado < 0:
         lista_sorteados = []
         i = 0
